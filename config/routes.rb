1f36c237d3764ee054a1d9dfde2f0bf3572d8222
Lisa Mae <lisamae135b7@me.com>
2017-10-27 16:26:55 -0400

    created login and signup / made secure password encryption

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,17 @@ Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
 
 
+  resources :comments
+  resources :image_posts
+  resources :text_posts
+  resources :posts
+  resources :users
+  resources :sessions
+
+  get 'signup', to: 'users#new', as: 'signup'
+
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  # root 'posts#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026195518) do
+ActiveRecord::Schema.define(version: 20171027162823) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 20171026195518) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "first"
     t.string "last"
@@ -55,6 +54,7 @@ ActiveRecord::Schema.define(version: 20171026195518) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
