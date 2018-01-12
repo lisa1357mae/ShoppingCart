1264a563b737f2bc61bb1092332fd361d5f3d54b
Lisa Mae <lisamae135b7@me.com>
2018-01-11 22:37:32 -0500

    minor css fixes and migration resolution

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171029183448) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
     t.datetime "created_at", null: false
